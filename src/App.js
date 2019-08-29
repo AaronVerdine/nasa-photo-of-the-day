04d8d003c5245393138dae6af0a3c7828bb455cd
Your name <a.m.verdine@gmail.com>
2019-08-29 14:01:08 -0400

    Update App.js with axios get request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import PhotoCard from "./components/PhotoCard";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res => {
+      console.log(res.data);
+      setData(res.data)
+    
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+  
+   
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <PhotoCard title={data.title}
+                            url={data.url}
+                            explanation={data.explanation}   />
     </div>
   );
 }
